3c115fc401b8d9c135bb624744925536563dd9bd
Namghjk <namsgu123@gmail.com>
2023-01-19 23:40:40 +0700

    second commit

--- ApiApp/Controllers/TabBar/TabViewController.swift
+++ ApiApp/Controllers/TabBar/TabViewController.swift
@@ -12,7 +12,38 @@ class TabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-
+        setUpTabs()
     }
+    
+    private func setUpTabs(){
+        
+        let charactersVC = CharacterViewController()
+        let EpisodeVC = EpisodeViewController()
+        let SettingVC = SettingViewController()
+        let LocationVC = LocationViewController()
+        
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        EpisodeVC.navigationItem.largeTitleDisplayMode = .automatic
+        SettingVC.navigationItem.largeTitleDisplayMode = .automatic
+        LocationVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav3 = UINavigationController(rootViewController: EpisodeVC)
+        let nav4 = UINavigationController(rootViewController: SettingVC)
+        let nav2 = UINavigationController(rootViewController: LocationVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Character", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Episode", image: UIImage(systemName: "tv"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Setting", image: UIImage(systemName: "gear"), tag: 4)
+        nav4.tabBarItem = UITabBarItem(title: "Location", image: UIImage(systemName: "globe"), tag: 3)
+        
+        for nav in [nav1,nav2,nav3,nav4]{
+            nav.navigationBar.prefersLargeTitles
+            = true
+        }
+        
+        setViewControllers([nav1,nav2,nav3,nav4], animated: true)
+    }
+    
 
 }
