746fbcb1dfd29856dff9e0b71eec1dbf09c62f88
Namghjk <namsgu123@gmail.com>
2023-01-19 23:29:36 +0700

    first commit

---
+++ ApiApp/Controllers/TabBar/TabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ApiApp
+//
+//  Created by Nam Pham on 19/01/2023.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+
+}
---
+++ ApiApp/ViewModel/Episode.swift
@@ -0,0 +1,8 @@
+//
+//  Episode.swift
+//  ApiApp
+//
+//  Created by Nam Pham on 19/01/2023.
+//
+
+import Foundation
